27ba7c010b95efe32fce6420b8b28404b8f1070b
Jun <junaidvayoli@gmail.com>
2020-07-27 03:26:38 +0900

    include bs4

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import requests
 import time
 import os
+from bs4 import BeautifulSoup
 
 urls = []
 # urls.append({"name": "natboardhome", "url": "https://natboard.edu.in/"})
 # urls.append({"name": "ceeKerala", "url": "https://cee.kerala.gov.in/main.php"})
 # urls.append({"name": "arogya_karnataka", "url": "http://arogya.karnataka.gov.in/sast/fms/view.php"})
-urls.append({"name": "vande_bharat_phase_5", "url": "https://mea.gov.in/phase-5.htm"})
+urls.append({"name": "vande_bharat_phase_5", "url": "https://mea.gov.in/phase-5.htm", "class": "tableOut"})
 
 
 BOT_TOKEN = os.environ['BOT_TOKEN']
@@ -28,11 +29,17 @@ def check_now(a):
 		f.close()
 	except:
 		content = ""
-	# print(content)
 	result = res.text
-	if content != result:
+	soup_new = BeautifulSoup(result, 'html.parser')
+	tableOut_new = soup_new.find('div', class_=url["class"])
+	# print(tableOut_new)
+	soup_old = BeautifulSoup(content, 'html.parser')
+	tableOut_old = soup_old.find('div', class_=url["class"])
+	# print(tableOut_old)
+	if tableOut_old.prettify() != tableOut_new.prettify():
+		# if content != result:
 		print("Content changed for: "+url["name"])
-		requests.get(url_update + url["name"] + "\n\n" + url["url"])
+		# requests.get(url_update + url["name"] + "\n\n" + url["url"])
 		f = open(file_path, "w+")
 		f.write(result)
 		f.close()
